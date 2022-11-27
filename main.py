bb8913e68849791d78c5b9a6bc61e64f1e4d1710
Joel Jacob <joeljacobdev@gmail.com>
2022-11-27 17:13:33 +0530

    added basic gunicorn conf

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import asyncio
+import uvloop
 from importlib import import_module
 from fastapi import FastAPI
 from app.config import get_config
 from app.fastapi import route as api_route
 
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 setting = getattr(import_module(get_config()), 'Setting')()
 
 app = FastAPI()
