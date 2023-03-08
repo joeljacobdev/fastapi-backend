3337a264ffa6b9aca3b48bfc1627b242aa5de4fc
Joel Jacob <joeljacobdev@gmail.com>
2023-03-08 10:10:35 +0530

    Add sqlalchemy example

---
+++ app/example/sqlalchemy/api.py
@@ -0,0 +1,92 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy import func, Integer
+from sqlalchemy.future import select
+from sqlalchemy.orm import joinedload
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.example.sqlalchemy import schemas as app_schemas
+from app.example.sqlalchemy.setup import get_db_session
+
+router = APIRouter()
+
+
+@router.get("/setup")
+async def setup(session: AsyncSession = Depends(get_db_session)):
+    await session.execute(
+        app_schemas.College.__table__.insert(),
+        [{"name": f"College {i}"} for i in range(10)],
+    )
+    result = await session.execute(select(app_schemas.College.id))
+    college_ids = result.scalars().all()
+    department_batches = []
+    for college_id in college_ids:
+        department_batches.extend(
+            [{"college_id": college_id, "batch": batch} for batch in range(2010, 2023)]
+        )
+    await session.execute(
+        app_schemas.DepartmentBatch.__table__.insert(),
+        department_batches,
+    )
+    result = await session.execute(select(app_schemas.DepartmentBatch.id))
+    department_batch_ids = result.scalars().all()
+    courses = []
+    for dept_batch_id in department_batch_ids:
+        courses.extend(
+            [
+                {"name": f"Course {i}", "department_batch_id": dept_batch_id}
+                for i in range(2)
+            ]
+        )
+    await session.execute(
+        app_schemas.Course.__table__.insert(),
+        courses,
+    )
+    result = await session.execute(select(app_schemas.Course.id))
+    course_ids = result.scalars().all()
+    students = [app_schemas.Student(name=f"Student {i}") for i in range(100)]
+    session.add_all(students)
+    return []
+
+
+@router.get("/m2m")
+async def m2m(session: AsyncSession = Depends(get_db_session)):
+    student_id = 1
+    statement = (
+        select(app_schemas.Student)
+        .where(app_schemas.Student.id == student_id)
+        .options(
+            joinedload(app_schemas.Student.student_courses).options(
+                joinedload(app_schemas.StudentCourse.course)
+            )
+        )
+    )
+    result = await session.execute(statement)
+    return result.unique().all()
+
+
+@router.get("/array_agg")
+async def array_agg(session: AsyncSession = Depends(get_db_session)):
+    college_id = 2
+    statement = (
+        select(
+            app_schemas.College.name,
+            func.array_agg(app_schemas.Course.id, type_=ARRAY(Integer)).label(
+                "course_ids"
+            ),
+        )
+        .outerjoin(
+            app_schemas.DepartmentBatch,
+            app_schemas.DepartmentBatch.college_id == app_schemas.College.id,
+        )
+        .outerjoin(
+            app_schemas.Course,
+            app_schemas.Course.department_batch_id == app_schemas.DepartmentBatch.id,
+        )
+        .where(app_schemas.College.id == college_id)
+        .group_by(app_schemas.College.id)
+    )
+    result = await session.execute(statement)
+    result = result.one()
+    course_ids = result[1] if result else []
+    return {"college_id": college_id, "course_ids": course_ids}
---
+++ app/example/sqlalchemy/setup.py
@@ -0,0 +1,36 @@
+import asyncio
+from sqlalchemy.orm import declarative_base, sessionmaker, scoped_session
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+
+Base = declarative_base()
+
+POSTGRES_DSN = "postgresql+asyncpg://joel:joeljacob@0.0.0.0:9500/fastapi-sqlalchemy"
+# we allow 210 connections in db - and we run with 2 worker and each worker we have a single app having 80+20 connections
+# max conn we will use = 2*(80+20) = 200 (we can at max increase the connections by 5)
+engine = create_async_engine(
+    POSTGRES_DSN, echo=False, future=True, pool_size=80, max_overflow=20
+)
+async_session = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False, future=True
+)
+""":type: sqlalchemy.orm.AsyncSession"""
+
+
+async def get_db_session() -> AsyncSession:
+    async with async_session() as session:
+        try:
+            yield session
+        except Exception as e:
+            raise
+        finally:
+            # TODO - only do this if there is any write made
+            await session.commit()
+
+
+async def init_database():
+    # TODO if anything needs to be created
+
+    async with engine.begin() as conn:
+        # TODO - only to be used for local dev, till migrations are setup
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
